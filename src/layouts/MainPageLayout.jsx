51143cc7e73b5f0557e88a021e53185769695bd3
Tsagiannoudis <i.tsagiannoudis@gmail.com>
2025-06-26 21:12:17 +0300

    install routes only

--- src/layouts/MainPageLayout.jsx
+++ src/layouts/MainPageLayout.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function MainPageLayout({ controlpanel, mapcontainer }) {
     return (
-        <div className='flex flex-col lg:flex-row h-screen bg-gray-100'>
+        <div className='flex flex-col-reverse lg:flex-row h-screen bg-gray-100'>
             {/* left side panel */}
             <aside className='w-full lg:w-1/4 bg-gray-50 p-6 border-b lg:border-b-0 lg:border-r border-gray-200 shadow-inner overflow-y-auto flex-shrink-0'>
                 {controlpanel}
